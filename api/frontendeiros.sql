556e853d0908732566aeccea54d73ff1d5eb976f
Carlos Figueiredo <carlosfigueiredo@souunisuam.com.br>
2023-04-25 21:39:04 -0300

    2023.04.25.1

--- api/frontendeiros.sql
+++ api/frontendeiros.sql
@@ -1,6 +1,6 @@
 -- ---------------------------------
 -- FrontEndeiros - Banco de dados
--- By Carlos Figueiredo
+-- By Luferat
 -- MIT License
 -- 
 -- Modela o banco de dados da API do aplicativo.
@@ -16,7 +16,6 @@ CREATE DATABASE frontendeiros CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 
 -- Seleciona o banco de dados para as próximas interações.
 USE frontendeiros;
-
 
 -- Cria tabela dos contatos → contacts.
 CREATE TABLE contacts (
@@ -35,6 +34,7 @@ CREATE TABLE users (
     user_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     user_name VARCHAR(255) NOT NULL,
     user_email VARCHAR(255) NOT NULL,
+    user_password VARCHAR(63) NOT NULL,
     user_photo VARCHAR(255) COMMENT 'URL da imagem.',
     user_bio VARCHAR(255),
     user_birth DATE,
@@ -52,7 +52,7 @@ CREATE TABLE articles (
     art_resume VARCHAR(127) NOT NULL,
     art_content TEXT NOT NULL,
     art_views INT DEFAULT 0,
-    art_status ENUM('on', 'off', 'del')
+    art_status ENUM('on', 'off', 'del') DEFAULT 'on',
     FOREIGN KEY (art_author) REFERENCES users (user_id)
 );
 
@@ -87,40 +87,50 @@ INSERT INTO contacts
     ( name, email, subject, message )
 VALUES 
     ('Joca da Silva', 'joca@silva.com', 'Erro', 'Não consigo me cadastrar.'),
-    ('Setembrino', 'set@brino.com.br', 'biscoito', 'Biscoit não é bolacha.'),
+    ('Setembrino', 'set@brino.com.br', 'biscoito', 'Biscoito não é bolacha.'),
     ('Maricleuza', 'mari@cleuza.net', 'bolacha', 'Bolacha é biscoito são a mesma coisa.');
 
 -- Insere dados na tabela 'users'.
 INSERT INTO users (
     user_name,
     user_email,
+    user_password,
     user_photo,
     user_bio,
-    user_birth
+    user_birth,
+    user_type
 ) VALUES (
     'Joca da Silva',
     'joca@silva.com',
+    SHA1('Senha_123'),
     'https://randomuser.me/api/portraits/men/33.jpg',
     'Construtor, programador e enrolador',
-    '2000-10-28'
+    '2000-10-28',
+    'author'
 );
 
 -- Insere dados na tabela 'users'.
 -- Somente campos "not null".
 INSERT INTO users (
     user_name,
-    user_email
+    user_email,
+    user_password
 ) VALUES (
     'Setembrino',
-    'set@brino.com.br'
+    'set@brino.com.br',
+    SHA1('Senha_123')
 );
 
 -- Insere dados na tabela 'users'.
 -- Somente o 'name'.
 INSERT INTO users (
-    user_name
+    user_name,
+    user_email,
+    user_password
 ) VALUES (
-    'Hermenilda'
+    'Hermenilda',
+    'herme@nilda.com',
+    SHA1('Senha_123')
 );
 
 -- Insere artigos na tabela 'articles'.
@@ -155,4 +165,29 @@ INSERT INTO articles (
 
 -- Atividade
 -- 1) Insira pelo menos mais 2 artigos.
+
+INSERT INTO articles (
+    art_author,
+    art_title,
+    art_thumbnail,
+    art_resume,
+    art_content
+) VALUES (
+    '1',
+    'Mais um artigo para o blog',
+    'https://picsum.photos/199',
+    'Resumo do artigo que va aparecer no blog',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas a ultrices leo. Vivamus in suscipit quam. Sed posuere erat non massa vehicula laoreet.'
+), (
+    '1',
+    'Próximo artigo para do blog',
+    'https://picsum.photos/198',
+    'Esse é só mais um artigo fake',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas a ultrices leo. Vivamus in suscipit quam. Sed posuere erat non massa vehicula laoreet.'
+);
+
 -- 2) Insira pelo menos 2 comentários de usuários diferentes para um mesmo artigo.
+
+INSERT INTO comments ( cmt_author, cmt_article, cmt_comment ) VALUES
+( '2', '2', 'Comentando um comentário comentado.'),
+( '3', '2', 'Comentártio da Hermenilda neste mesmo artigo.');
