4e3dd0d553ba87b7b7393897ab64615f92d29341
Carlos Figueiredo <carlosfigueiredo@souunisuam.com.br>
2023-04-28 21:38:59 -0300

    2023.04.28.1
    criação do login/logout.

--- fb.js
+++ fb.js
@@ -1,19 +1,16 @@
 /**
- * Integração com o Firebase.com
+ * Configuração do Firebase
  * By Luferat
- * MIT License
+ * MIT License 
  **/
 
-// Configuração do Firebase para o site do prof. Luferat.
-
-// Importa o "core" do Firebase.
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-app.js";
-
-// Importa o Authentication do Firebase.
-import { getAuth, onAuthStateChanged, GoogleAuthProvider, signInWithPopup, signInWithRedirect } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-auth.js";
-
-// NÃO USE ESTE CÓDIGO NO SEU APLICATIVO.
-const firebaseConfig = {
+/**
+ * Configurações so Firebase
+ * 
+ * IMPORTANTE!
+ * Troque os valores de 'firebaseConfig' pelos dados do SEU FIREBASE!
+ **/
+ const firebaseConfig = {
     apiKey: "AIzaSyDEGPx7u46gGlvA-NxFJMrJRpNAD9DFtCs",
     authDomain: "frontendeiros-carlos.firebaseapp.com",
     projectId: "frontendeiros-carlos",
@@ -21,109 +18,11 @@ const firebaseConfig = {
     messagingSenderId: "676387430883",
     appId: "1:676387430883:web:8d58f7f87dde1ecd4d00d6"
   };
- 
-// Initializa o Firebase.
-const fbapp = initializeApp(firebaseConfig);
+// Incializa o Firebase
+firebase.initializeApp(firebaseConfig);
 
-// Inicializa o mecanismo de autenticação.
-const auth = getAuth();
+// Incializa o Firebase Authentication
+const auth = firebase.auth();
 
-// Especifica o provedor de autenticação.
-const provider = new GoogleAuthProvider();
-
-// Observa o status de autenticação do usuário.
-onAuthStateChanged(auth, (user) => {
-    if (user) {
-        sessionStorage.userData = JSON.stringify({
-            name: user.displayName,
-            email: user.email,
-            photo: user.photoURL,
-            uid: user.uid
-        })
-    } else {
-        delete sessionStorage.userData
-    }
-});
-
-// Initializa o Firebase.
-const fbapp = initializeApp(firebaseConfig);
-
-// Inicializa o mecanismo de autenticação.
-const auth = getAuth();
-
-// Especifica o provedor de autenticação.
-const provider = new GoogleAuthProvider();
-
-// Observa o status de autenticação do usuário.
-onAuthStateChanged(auth, (user) => {
-    if (user) {
-        sessionStorage.userData = JSON.stringify({
-            name: user.displayName,
-            email: user.email,
-            photo: user.photoURL,
-            uid: user.uid
-        })
-    } else {
-        delete sessionStorage.userData
-    }
-});
-
-// Initializa o Firebase.
-const fbapp = initializeApp(firebaseConfig);
-
-// Inicializa o mecanismo de autenticação.
-const auth = getAuth();
-
-// Especifica o provedor de autenticação.
-const provider = new GoogleAuthProvider();
-
-// Observa o status de autenticação do usuário.
-onAuthStateChanged(auth, (user) => {
-    console.log(user)
-    if (user) {
-        sessionStorage.userData = JSON.stringify({
-            name: user.displayName,
-            email: user.email,
-            photo: user.photoURL,
-            uid: user.uid,
-            created: user.metadata.createdAt,
-            lastLogin: user.metadata.lastLoginAt 
-        })
-    } else {
-        delete sessionStorage.userData
-    }
-});
-
-// Executa a jQuery quando o documento estiver pronto.
-$(document).ready(myFirebase)
-
-function myFirebase() {
-
-    // Detecta cliques no botão de login.
-    $('#navUser').click(login)
-}
-
-// Função que processa cliques no botão login/profile.
-function login() {
-
-    // Se não está logado...
-    if (!sessionStorage.userData) {
-
-        // Faz login usando popup.
-        signInWithPopup(auth, provider)
-
-            // Se logou corretamente.
-            .then(() => {
-
-                // Redireciona para a 'home'.
-                location.href = '/home'
-            })
-
-        // Se está logado..
-    } else
-
-        // Redireciona para 'profile'.
-        location.href = '/profile'
-
-    return false
-}
+// Define o provedor de autenticação
+var provider = new firebase.auth.GoogleAuthProvider();
